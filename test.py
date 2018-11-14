d4eac10ba48672fc190eb0986533347dbbb6a10a
millermmx <miller.kinlin@metamarkets.com>
2018-11-14 15:45:42 -0500

    yup

--- test.py
+++ test.py
@@ -725,29 +725,34 @@ import heapq
 # class Solution:
 #     # @param A : list of strings
 #     # @return a list of strings
-#     def prefix(self, A):
-#         tree = [0, {}]
-#         for s in A:
-#             node = tree
-#             node[0] += 1
-#             for c in s:
-#                 node = node[1].setdefault(c, [0, {}])
-#                 node[0] += 1
-#         l = []
-#         for s in A:
-#             prefix = ''
-#             node = tree
-#             for c in s:
-#                 if node[0] <= 1:
-#                     l.append(prefix)
-#                     break
-#                 prefix += c
-#                 node = node[1][c]
-#             else:
-#                 l.append(s)
-#         return l
+def prefix(A):
+    tree = [0, {}]
+    for s in A:
+        node = tree
+        node[0] += 1
+        for c in s:
+            node = node[1].setdefault(c, [0, {}])
+            print(node)
+            node[0] += 1
+    print("tree", tree)
+    l = []
+    for s in A:
+        prefix = ''
+        node = tree
+        for c in s:
+            if node[0] <= 1:
+                l.append(prefix)
+                break
+            prefix += c
+            node = node[1][c]
+        else:
+            l.append(s)
+    return l
 
 # Input is a list of strings, returns prefixes
+l = ["hi", "hip", "hello", "z"]
+
+print(prefix(l))
 
 
 
